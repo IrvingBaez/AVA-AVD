0863b95c5a97f0f50703b328f1e4e0881c2ad3c9
IrvingBaez <irvingbaezc@gmail.com>
2024-02-28 17:08:00 -0600

    Make sure env exists in pytorch_run.sh

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -4,9 +4,10 @@ env_name='ava-avd'
 
 if conda info --envs | grep -q ${env_name}
 then
-    conda activate ${env_name} >> ../log_environment.txt
+    conda activate ${env_name} >> log_environment.txt
 else
-    bash ../environment.sh >> ../log_environment.txt
+    bash ../environment.sh >> log_environment.txt
+    conda activate ${env_name} >> log_environment.txt
 fi
 
 CUDA_VISIBLE_DEVICES=`/home/gkumar/scripts/free-gpu` python  "$@"
