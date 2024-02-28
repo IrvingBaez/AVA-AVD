83852667f8cc449ed64c4d82e6e15ea2cd0659c2
IrvingBaez <irvingbaezc@gmail.com>
2024-02-28 17:34:20 -0600

    Make sure env exists in pytorch_run.sh

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -4,8 +4,10 @@ env_name='ava-avd'
 
 if conda info --envs | grep -q ${env_name}
 then
+    echo 'env already exists' >> log_environment.txt
     conda activate ${env_name} >> log_environment.txt
 else
+    echo 'creating env' >> log_environment.txt
     bash ../environment.sh >> log_environment.txt
     conda activate ${env_name} >> log_environment.txt
 fi
