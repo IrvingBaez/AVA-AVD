36982d4731612b876b1eb2e3dd57edb3781f6acc
IrvingBaez <irvingbaezc@gmail.com>
2024-02-28 18:54:00 -0600

    Make sure env exists in pytorch_run.sh

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -1,6 +1,6 @@
 # Activate your virtual environment
 source ~/miniconda3/etc/profile.d/conda.sh
-bash ../environment.sh
+bash environment.sh
 env_name='ava-avd'
 
 CUDA_VISIBLE_DEVICES=`/home/gkumar/scripts/free-gpu` python  "$@"
