e50428d19e76b8eddf09730b31a8d2a602b72964
IrvingBaez <irvingbaezc@gmail.com>
2024-02-28 18:46:29 -0600

    Make sure env exists in pytorch_run.sh

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -1,15 +1,6 @@
 # Activate your virtual environment
 source ~/miniconda3/etc/profile.d/conda.sh
+bash ../environment.sh
 env_name='ava-avd'
-
-if conda info --envs | grep -q ${env_name}
-then
-    echo 'env already exists' >> log_environment.txt
-    conda activate ${env_name} >> log_environment.txt
-else
-    echo 'creating env' >> log_environment.txt
-    bash ../environment.sh >> log_environment.txt
-    conda activate ${env_name} >> log_environment.txt
-fi
 
 CUDA_VISIBLE_DEVICES=`/home/gkumar/scripts/free-gpu` python  "$@"
--- environment.sh
+++ environment.sh
@@ -5,60 +5,46 @@ source ~/miniconda3/etc/profile.d/conda.sh
 if conda info --envs | grep -q ${env_name}
 then
   echo "Environment ${env_name} already exists"
-
-  # If env existis, just activate it
-  # eval "$(conda shell.bash hook)"
-  # conda activate ${env_name}
 else
   # Create env if it doesn't exists.
   echo "Creating environment ${env_name}"
   conda create -y -n ${env_name} python=3.9
-
-  # Activate env
-  eval "$(conda shell.bash hook)"
-  conda activate ${env_name}
-
-  # Download retinaface weights
-  pip install gdown
-  gdown '1ZggExiXzl1Uiq8eOYoCnQKQkN0bY1CHi';
-  mv R50-0000.params dataset/third_party/insightface/detection/retinaface/
-
-  # Install dependencies
-  conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.2 -c pytorch -y
-  conda install -c conda-forge nccl -y
-
-  pip3 install "setuptools<58.0.0"
-  pip3 install mxnet-cu102
-  pip3 install scikit-image
-  pip3 install Cython
-  pip3 install opencv-python-headless==4.5.1.48
-  # pip3 install rcnn
-  pip3 install -r requirements.txt
-  pip3 install -r dataset/third_party/insightface/requirements.txt
-
-  cd dataset/third_party/insightface/detection/retinaface/
-  make
 fi
 
-# At this point, the env with all the necesary libraries is active.
+# Activate env
+eval "$(conda shell.bash hook)"
+conda activate ${env_name}
 
-# Adjustments for CUDA and cuDNN paths:
-# Determine the path to the active conda environment.
-# CONDA_ENV_PATH=$(conda info --envs | grep -Po "${env_name}\K.*" | sed 's: ::g')
+# Check if PyTorch and torchvision are installed, if not, install them
+if ! conda list | grep -q "pytorch"
+then
+  conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.2 -c pytorch -y
+fi
 
-# Assuming CUDA and cuDNN are installed within the conda environment, set the environment variables.
-# export LD_LIBRARY_PATH=${CONDA_ENV_PATH}/lib:$LD_LIBRARY_PATH
-# export CUDA_HOME=${CONDA_ENV_PATH}
-# export PATH=$CUDA_HOME/bin:$PATH
+# Install other dependencies
+conda install -c conda-forge nccl -y
 
-# Excecute code.
-# export PYTHONPATH=./dataset/third_party/insightface/detection/retinaface:$PYTHONPATH
+pip3 install gdown
+gdown '1ZggExiXzl1Uiq8eOYoCnQKQkN0bY1CHi';
+mv R50-0000.params dataset/third_party/insightface/detection/retinaface/
 
-# task_file=$@
-# clsp_grid_wrapper/submit_grid.sh $task_file
+pip3 install "setuptools<58.0.0"
+pip3 install mxnet-cu102
+pip3 install scikit-image
+pip3 install Cython
+pip3 install opencv-python-headless==4.5.1.48
+pip3 install --upgrade setuptools wheel
+pip3 install -r dataset/third_party/insightface/requirements.txt
+pip3 install -r requirements.txt
 
-# For test:
-# ./scheduele.sh clsp_grid_wrapper/test_pytorch.py
+# Build retinaface
+cd dataset/third_party/insightface/detection/retinaface/
+make
 
-# For preprocessing:
-# ./scheduele.sh dataset/scripts/preprocessing.py dataset/videos
+# Environment Variables adjustment
+CONDA_ENV_PATH=$(conda info --envs | grep -Po "${env_name}\K.*" | sed 's: ::g')
+export LD_LIBRARY_PATH=${CONDA_ENV_PATH}/lib:$LD_LIBRARY_PATH
+export CUDA_HOME=${CONDA_ENV_PATH}
+export PATH=$CUDA_HOME/bin:$PATH
+
+# At this point, the env with all the necessary libraries is active.
