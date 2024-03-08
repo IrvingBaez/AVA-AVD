e0debe5925f556ff380dc9ea59adb0524d986633
Irving Baez <irvingbaezc@gmail.com>
2024-03-07 20:20:59 -0600

    changed cuda visible devices function

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -2,4 +2,7 @@
 source /export/b03/carlosc/miniconda3/etc/profile.d/conda.sh
 conda activate ava-avd
 
-CUDA_VISIBLE_DEVICES=`/home/gkumar/scripts/free-gpu` python  "$@"
+# export PATH="/export/b03/carlosc/miniconda3/envs/ava-avd/bin:$PATH"
+# source activate /export/b03/carlosc/miniconda3/envs/ava-avd
+
+CUDA_VISIBLE_DEVICES=$(free-gpu -n 1) python  "$@"
