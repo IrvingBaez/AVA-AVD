986238462db6c58068a7587fd210bd25ec8734a7
Irving Baez <irvingbaezc@gmail.com>
2024-04-14 20:43:31 -0600

    Chaged test and train file permissions

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-export PYTHONPATH=./model \
-CUDA_VISIBLE_DEVICES=0 \
+export PYTHONPATH=./model
+export CUDA_VISIBLE_DEVICES=0
 clsp_grid_wrapper/submit_grid model/mmsc_exp/predict.py \
   config=projects/token/avaavd.yaml \
   datasets=avaavd \
