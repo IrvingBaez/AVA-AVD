6fca5e63aeee65d01fab1abb9a101686ef76ee33
Irving Baez <irvingbaezc@gmail.com>
2024-04-14 23:53:40 -0600

    Testing test.sh paths configuration

--- clsp_grid_wrapper/pytorch_run.sh
+++ clsp_grid_wrapper/pytorch_run.sh
@@ -5,5 +5,6 @@ conda activate ava-avd
 # export PATH="/export/b03/carlosc/miniconda3/envs/ava-avd/bin:$PATH"
 # source activate /export/b03/carlosc/miniconda3/envs/ava-avd
 
-export PYTHONPATH=../model
+export PYTHONPATH=/export/c03/carlosc/ibaez/AVA-AVD/model
+echo "PYTHONPATH set to: $PYTHONPATH"
 CUDA_VISIBLE_DEVICES=$(free-gpu -n 1) python  "$@"
